9b42572d9dec179ad817c09eebccdc01de7d6253
OlgaWa <olga.wacholc@gmail.com>
2021-04-13 20:43:28 +0200

    Adding file saving from python to csv

---
+++ python_connect.py
@@ -0,0 +1,11 @@
+import csv
+
+
+name = input('What is your name?')
+skin_type = input('What is your skin type?')
+
+
+with open('database.csv', mode='a', newline='') as database:
+    fieldnames = ['Name', 'Skin typed']
+    csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow([name,skin_type])
